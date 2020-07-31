29a4509250f1d7554d6f8e93cf197d2750571df7
Justin Mathew <jmathew2111@yahoo.com>
2020-07-31 22:58:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,4 +1,4 @@
- birthday_kids = {
+birthday_kids = {
    "Timmy" => 9, 
    "Sarah" => 6, 
   "Amanda" => 27
@@ -6,7 +6,9 @@
       def happy_birthday(birthday_kids)
       birthday_kids.each do |kids_name, age|
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+        if kids_name < 12
+        else 
+          puts birthday_kids 
       end
+    end 
     end
-
-
